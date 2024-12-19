9fd7513878dec67e138aa16ff9f5b6dc0f6da567
b0r3d0m <b0r3d0mness@gmail.com>
2024-12-19 08:24:28 -0800

    - Replace Tabs with Spaces

--- src/extension.ts
+++ src/extension.ts
@@ -11,17 +11,17 @@ function enterText(text: string) {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('todoot.insertQuote', () => {
-		axios.get('https://dummyjson.com/quotes/random')
-			.then(function (response) {
-				enterText('// TODO: ' + response.data.quote);
-			})
-			.catch(function (error) {
-				vscode.window.showInformationMessage(error);
-			})
-	});
+    const disposable = vscode.commands.registerCommand('todoot.insertQuote', () => {
+        axios.get('https://dummyjson.com/quotes/random')
+            .then(function (response) {
+                enterText('// TODO: ' + response.data.quote);
+            })
+            .catch(function (error) {
+                vscode.window.showInformationMessage(error);
+            })
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
